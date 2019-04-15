6799d1cf1d25564bb1d1ba3a7649dbedbb6ed99c
Mark <34401402+DukeFR@users.noreply.github.com>
2019-04-15 10:52:29 +0200

    Add files via upload

---
+++ Arrete.cpp
@@ -0,0 +1,32 @@
+#include "Arrete.h"
+
+#include <iostream>
+
+Arrete::Arrete(Sommet* s1,Sommet* s2,int id,int p1,int p2):depart{s1},arrivee{s2},m_id{id},m_poids1{p1},m_poids2{p2}
+{
+}
+
+void Arrete::afficherArrete() const
+{
+    depart->afficherData();
+    arrivee->afficherData();
+    std::cout<<"--------------------------------------------"<<std::endl;
+    std::cout<<"id:"<<m_id<<" "<<std::endl;
+    std::cout<<"poids1:"<<m_poids1<<" "<<std::endl;
+    std::cout<<"poids2"<<m_poids2<<" "<<std::endl;
+}
+
+Sommet* Arrete::getDepart()
+{
+    return depart;
+}
+
+Sommet* Arrete::getArrivee()
+{
+    return arrivee;
+}
+
+int Arrete::getPoids()
+{
+    return m_poids1;
+}
---
+++ Graph.h
@@ -0,0 +1,28 @@
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+#include "Arrete.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+class graphe
+{
+    public:
+        graphe(std::string);
+        //~graphe();
+        void afficher() const;
+
+    protected:
+
+    private:
+        /// Le réseau est constitué d'une collection de sommets
+        std::unordered_map<int,Sommet*> m_sommets;//stockée dans une map (clé=id du sommet, valeur= pointeur sur le sommet)
+        std::vector<Arrete*> m_arrete;
+
+};
+
+
+
+#endif // GRAPH_H_INCLUDED
